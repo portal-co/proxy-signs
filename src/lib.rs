0a80719433f5fc7a80ecffe59052765862fbaf85
Graham Kelly <gkgoat6700@gmail.com>
2025-07-27 05:19:25 +0000

    init

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+pub static PROXY_SIGNS: &[&str] = &[
+    "pan_eval",
+    "$scramjet",
+    "$scram",
+    "__uv",
+    "__PortalEnterprise",
+    "_WBWombat",
+    "_WB",
+    "_wb_wombat",
+    "_wb",
+];
